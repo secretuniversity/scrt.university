b01545a0bd80840be578289e744a79074d5020c6
Alex Werner <alewern@pm.me>
2022-07-25 09:30:42 -0500

    task: extracts features, footer, and section header components

---
+++ src/lib/sectionHeader.svelte
@@ -0,0 +1,17 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="bg-white">
+	<div class="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+		<div class="text-center">
+			<h2 class="text-base font-semibold uppercase tracking-wide text-indigo-600">Pricing</h2>
+			<p
+				class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl"
+			>
+				Take control of your team.
+			</p>
+			<p class="mx-auto mt-5 max-w-xl text-xl text-gray-500">
+				Start building for free, then add a site plan to go live. Account plans unlock additional
+				features.
+			</p>
+		</div>
+	</div>
+</div>
