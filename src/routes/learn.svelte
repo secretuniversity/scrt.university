e0424f47f1fa2ed52ad8c9dad71fc8178a8ff6f2
Alex Werner <alewern@pm.me>
2022-07-26 10:42:47 -0500

    task: begins adding similar markup to learn pages

---
+++ src/routes/learn.svelte
@@ -0,0 +1,14 @@
+<!-- Learn Page Header -->
+<div class="bg-gray-800">
+	<div class="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:flex lg:justify-between lg:px-8">
+		<div class="max-w-xl">
+			<h2 class="text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
+				Learn
+			</h2>
+			<p class="mt-5 text-xl text-gray-400">
+				Learn to build on Secret Network using our courses Secret Pathways or dive deep into code
+				with Secret Boxes
+			</p>
+		</div>
+	</div>
+</div>
