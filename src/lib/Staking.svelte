e00150f71f22e02351910497a83b291f4ad88fa7
Alex Werner <alewern@pm.me>
2022-07-27 10:39:38 -0500

    task: adds basic markup for Staking & Stats components

---
+++ src/lib/Staking.svelte
@@ -0,0 +1,12 @@
+<div class="container mx-auto px-4 sm:px-6 lg:py-16 lg:px-8">
+	<div class="overflow-hidden rounded-lg bg-white px-4 py-6 shadow lg:px-8 lg:py-4">
+		<!-- Content goes here -->
+		<h2 class="text-lg font-medium leading-6 text-gray-900">Support</h2>
+		<p class="mt-2 text-base text-gray-500 lg:max-w-xl">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et nisi efficitur, consequat
+			neque nec, volutpat orci. Proin auctor finibus dui, sed condimentum urna cursus ac. Mauris
+			semper at ipsum eget interdum. Cras ullamcorper fringilla nunc, ut accumsan arcu accumsan ac.
+		</p>
+		<button class="lg:py-4">Learn more</button>
+	</div>
+</div>
