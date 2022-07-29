8077e6d73c864cdec2dc00b4271c469cabd0abe2
Alex Werner <alewern@pm.me>
2022-07-29 11:06:35 -0500

    task: adds markup for contribute page

--- src/lib/Staking.svelte
+++ src/lib/Staking.svelte
@@ -1,12 +1,34 @@
-<div class="container mx-auto px-4 sm:px-6 lg:py-16 lg:px-8">
+<div class="container mx-auto px-4 sm:px-6 lg:py-20 lg:px-16">
 	<div class="overflow-hidden rounded-lg bg-white px-4 py-6 shadow lg:px-8 lg:py-4">
 		<!-- Content goes here -->
-		<h2 class="text-lg font-medium leading-6 text-gray-900">Support</h2>
+		<h2 class="text-2xl font-semibold leading-6 text-gray-900">Stake With Us</h2>
 		<p class="mt-2 text-base text-gray-500 lg:max-w-xl">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et nisi efficitur, consequat
-			neque nec, volutpat orci. Proin auctor finibus dui, sed condimentum urna cursus ac. Mauris
-			semper at ipsum eget interdum. Cras ullamcorper fringilla nunc, ut accumsan arcu accumsan ac.
+			Secret University is committed to supporting developer growth and education on Secret Network.
+			By staking your SCRT with us, you are supporting the university's operating costs as well as
+			helping to fund developer, education-focused bounties.
 		</p>
-		<button class="lg:py-4">Learn more</button>
+
+		<div class="my-4 max-w-lg">
+			<label for="amount" class="block text-sm font-medium text-gray-700">Amount</label>
+			<div class="relative mt-1 rounded-md shadow-sm">
+				<input
+					type="text"
+					name="amount"
+					id="amount"
+					class="block w-full rounded-md border-gray-300 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+					placeholder="0.00"
+					aria-describedby="price-currency"
+				/>
+				<div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+					<span class="text-gray-500 sm:text-sm" id="amount-currency"> SCRT </span>
+				</div>
+			</div>
+		</div>
+
+		<button
+			type="button"
+			class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+			>Stake</button
+		>
 	</div>
 </div>
---
+++ src/routes/contribute.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import Head from '$lib/Head.svelte';
+	import Staking from '$lib/Staking.svelte';
+	const pageTitle = 'Contribute';
+</script>
+
+<Head {pageTitle} />
+
+<div class="mx-auto max-w-xl py-20">
+	<h1 class="text-center text-6xl font-bold text-gray-900">Learn to Earn</h1>
+	<p class="mt-2 text-center text-base text-gray-600">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel laoreet ipsum. Quisque et
+		nulla dapibus, vehicula odio mollis, vestibulum dolor. Cras hendrerit ipsum ut tempus
+		scelerisque.
+	</p>
+</div>
+
+<div class="flex justify-center gap-x-24">
+	<div class="lg:max-w-xs">
+		<div class="mx-auto h-24 w-24 bg-gray-500" />
+		<!-- <img src="" alt="" /> -->
+		<h2 class="mt-3 text-center text-lg font-semibold text-gray-900">Code</h2>
+		<p class="mt-1 text-center text-base text-gray-600">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</p>
+	</div>
+
+	<div class="lg:max-w-xs">
+		<div class="mx-auto h-24 w-24 bg-gray-500" />
+		<!-- <img src="" alt="" /> -->
+		<h2 class="mt-3 text-center text-lg font-semibold text-gray-900">Create</h2>
+		<p class="mt-1 text-center text-base text-gray-600">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</p>
+	</div>
+
+	<div class="lg:max-w-xs">
+		<div class="mx-auto h-24 w-24 bg-gray-500" />
+		<!-- <img src="" alt="" /> -->
+		<h2 class="mt-3 text-center text-lg font-semibold text-gray-900">Teach</h2>
+		<p class="mt-1 text-center text-base text-gray-600">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</p>
+	</div>
+</div>
+
+<div class="my-12 flex justify-center">
+	<button
+		type="button"
+		class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+		>Apply</button
+	>
+</div>
+
+<Staking />
