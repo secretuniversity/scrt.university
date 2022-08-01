0074ed4e940be598b6ea4c0272aa8eb82654d4b5
Alex Werner <alewern@pm.me>
2022-08-01 09:07:44 -0500

    task: adds markup for support page

---
+++ src/lib/support-ticket/SupportTicket.svelte
@@ -0,0 +1,122 @@
+<form class="space-y-8 divide-y divide-gray-200">
+	<div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+		<div>
+			<div>
+				<h3 class="text-lg font-medium leading-6 text-gray-900">Support Ticket</h3>
+				<p class="mt-1 max-w-2xl text-sm text-gray-500">
+					This ticket will be sent directly to the Secret University team.
+				</p>
+			</div>
+
+			<div class="mt-6 space-y-6 sm:mt-5 sm:space-y-5">
+				<div
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5"
+				>
+					<label for="subject" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+						Subject
+					</label>
+					<div class="mt-1 sm:col-span-2 sm:mt-0">
+						<input
+							id="subject"
+							name="subject"
+							type="text"
+							class="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						/>
+					</div>
+				</div>
+
+				<div
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5"
+				>
+					<label for="username" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+						Topic
+					</label>
+					<div class="mt-1 sm:col-span-2 sm:mt-0">
+						<div class="flex max-w-lg rounded-md shadow-sm">
+							<span
+								class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm"
+							>
+								scrt.university/
+							</span>
+							<input
+								type="text"
+								name="username"
+								id="username"
+								autocomplete="username"
+								class="block w-full min-w-0 flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5"
+				>
+					<label for="email" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+						Email address
+					</label>
+					<div class="mt-1 sm:col-span-2 sm:mt-0">
+						<input
+							id="email"
+							name="email"
+							type="email"
+							autocomplete="email"
+							class="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						/>
+					</div>
+				</div>
+
+				<div
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5"
+				>
+					<label for="about" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+						Description
+					</label>
+					<div class="mt-1 sm:col-span-2 sm:mt-0">
+						<textarea
+							id="about"
+							name="about"
+							rows="3"
+							class="block w-full max-w-lg rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						/>
+						<p class="mt-2 text-sm text-gray-500">Describe your problem in a couple sentences.</p>
+					</div>
+				</div>
+
+				<div
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5"
+				>
+					<label for="os" class="block text-sm font-medium text-gray-700">Operating System</label>
+					<div class="mt-1 sm:col-span-2 sm:mt-0">
+						<select
+							id="os"
+							name="os"
+							class="block w-full max-w-lg rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						>
+							<option>Windows</option>
+							<option>Mac OS</option>
+							<option>Linux</option>
+						</select>
+					</div>
+				</div>
+
+				<div />
+			</div>
+		</div>
+	</div>
+
+	<div class="pt-5">
+		<div class="flex justify-end">
+			<button
+				type="button"
+				class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				>Clear</button
+			>
+			<button
+				type="submit"
+				class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				>Submit</button
+			>
+		</div>
+	</div>
+</form>
---
+++ src/routes/support.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import Head from '$lib/Head.svelte';
+	import SectionHeader from '$lib/SectionHeader.svelte';
+	import SupportTicket from '$lib/support-ticket/SupportTicket.svelte';
+	const pageTitle = 'Support';
+</script>
+
+<Head {pageTitle} />
+
+<div class="grid grid-cols-1 items-center justify-items-center pt-16 pb-24 lg:grid-cols-2">
+	<div class="max-w-2xl flex-col py-16 lg:max-w-3xl lg:px-8">
+		<h1 class="self-start text-3xl font-bold text-gray-900">Need Support?</h1>
+		<p class="my-4 text-base text-gray-600">
+			Are you having trouble working through a course or guide. We're here to help. Submit a support
+			ticket and we will get back to you soon. If you need faster support, consider asking your
+			question in on Secret Network's many community channels.
+		</p>
+		<a
+			href="/support#community"
+			class="inline-flex items-center justify-self-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+			>Join The Community</a
+		>
+	</div>
+
+	<div class="w-9/12 overflow-hidden rounded-lg px-12 py-14 shadow-lg">
+		<SupportTicket />
+	</div>
+</div>
+
+<div id="community" class="mb-36">
+	<SectionHeader
+		title={'Secret Community'}
+		subtitle={'Join the community'}
+		description={'Find the answers to your questions in one of our many community channels.'}
+	/>
+
+	<div class="mx-auto mt-8 flex max-w-7xl gap-x-12">
+		<div class="lg:w-4/12">
+			<!-- <img src="" alt=""> -->
+			<h2 class="mb-3 text-center text-lg font-semibold text-gray-900">Chat on Discord</h2>
+			<p class="mb-4 text-center text-base text-gray-600">
+				Have real-time discussions with developers building on Secret Network
+			</p>
+			<a
+				href="#"
+				class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				>Join Discord</a
+			>
+		</div>
+
+		<div class="lg:w-4/12">
+			<!-- <img src="" alt=""> -->
+			<h2 class="mb-3 text-center text-lg font-semibold text-gray-900">Post on the Forum</h2>
+			<p class="mb-4 text-center text-base text-gray-600">
+				Have real-time discussions with developers building on Secret Network
+			</p>
+			<a
+				href="#"
+				class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				>Join Discussions</a
+			>
+		</div>
+
+		<div class="lg:w-4/12">
+			<!-- <img src="" alt=""> -->
+			<h2 class="mb-3 text-center text-lg font-semibold text-gray-900">Find us on Github</h2>
+			<p class="mb-4 text-center text-base text-gray-600">
+				Have real-time discussions with developers building on Secret Network
+			</p>
+			<a
+				href="#"
+				class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				>Open an Issue</a
+			>
+		</div>
+	</div>
+</div>
