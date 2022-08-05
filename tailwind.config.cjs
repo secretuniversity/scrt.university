8de18d91af465b5411155e5bf412e04c9da400b2
Alex Werner <alewern@pm.me>
2022-08-04 21:11:12 -0500

    task: adds secret network colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,35 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'dark-red': '#E8504A',
+				'dark-orange': '#EB8045',
+				'dark-yellow': '#FDD535',
+				'dark-turquoise-g': '#74B26E',
+				'dark-turquoise-b': '#4CBEB3',
+				'dark-blue': '#60A0DC',
+				'dark-purple': '#8B7AB0',
+				'dark-bronze': '#E8CDAE',
+
+				'light-red': '#EE7E7A',
+				'light-orange': '#EF9A6B',
+				'light-yellow': '#FEECA5',
+				'light-turquoise-g': '#BDDEBA',
+				'light-turquoise-b': '#A4DFD9',
+				'light-blue': '#ABCDED',
+				'light-purple': '#BDB3D0',
+				'light-cream': '#E8CDAE',
+
+				'dark-1': '#000000',
+				'dark-2': '#040506',
+				'dark-3': '#1A2128',
+				'dark-4': '#303C4A',
+
+				gray: '#B2BFCD',
+				'off-white': '#F9FAFB'
+			}
+		}
 	},
 	plugins: [require('@tailwindcss/forms')]
 };
