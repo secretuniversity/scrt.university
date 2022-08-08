70b2aebd26babd70a64e6a1b35ea4b2d4044ec59
Alex Werner <alewern@pm.me>
2022-08-08 09:00:31 -0500

    task: adds base styles to pathways page

--- src/routes/pathways/index.svelte
+++ src/routes/pathways/index.svelte
@@ -1,16 +1,25 @@
+<script lang="ts">
+	import Head from '$lib/Head.svelte';
+	const pageTitle = 'Pathways';
+</script>
+
+<Head {pageTitle} />
+
 <div class="mx-auto w-9/12 py-16">
 	<div class="max-w-3xl">
-		<h1 class="text-gray-900 mb-4 text-5xl font-bold">Secret Pathways</h1>
-		<p class="text-gray-600 mb-16 text-base">
+		<h1 class="mb-4 text-5xl font-bold text-white">Secret Pathways</h1>
+		<p class="mb-16 text-base text-gray">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque facilisis felis ut neque
 			fermentum eleifend. Integer ultricies tortor augue, id scelerisque justo varius vitae.
 			Vestibulum molestie at nisl ac cursus.
 		</p>
 	</div>
 
-	<div class="grid grid-cols-2 gap-y-12">
+	<div class="grid grid-cols-3 gap-12">
 		<a href="/pathways/welcome-to-secret-network">
-			<div class="max-w-lg overflow-hidden rounded-lg bg-white px-6 py-6 shadow lg:px-8 lg:py-8">
+			<div
+				class="max-w-lg overflow-hidden rounded-2xl bg-dark-4 px-6 py-6 shadow-lg lg:px-8 lg:py-8"
+			>
 				<div class="grid auto-rows-min">
 					<p
 						class="mb-4 inline-block justify-self-end rounded-full bg-blue-400 px-3 py-1 text-white"
@@ -19,10 +28,10 @@
 					</p>
 					<!-- <img src="" alt=""> -->
 					<div class="mx-auto mb-6 h-36 w-36 bg-slate-400" />
-					<h2 class="text-gray-900 mb-2 text-center text-lg font-semibold">
+					<h2 class="mb-2 text-center text-lg font-semibold text-white">
 						Welcome To Secret Network
 					</h2>
-					<p class="text-gray-600 text-center text-base">
+					<p class="text-center text-base text-gray">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed dapibus libero. Mauris
 						et orci vel nibh accumsan suscipit sit amet sit amet diam.
 					</p>
@@ -30,34 +39,32 @@
 			</div>
 		</a>
 
-		<div class="max-w-lg overflow-hidden rounded-lg bg-white px-6 py-6 shadow lg:px-8 lg:py-8">
+		<div class="max-w-lg overflow-hidden rounded-2xl bg-dark-4 px-6 py-6 shadow-lg lg:px-8 lg:py-8">
 			<div class="grid auto-rows-min">
 				<p class="mb-4 inline-block justify-self-end rounded-full bg-blue-400 px-3 py-1 text-white">
 					Beginner
 				</p>
 				<!-- <img src="" alt=""> -->
 				<div class="mx-auto mb-6 h-36 w-36 bg-slate-400" />
-				<h2 class="text-gray-900 mb-2 text-center text-lg font-semibold">
-					Writing Your First Secret Smart Contract
+				<h2 class="mb-2 text-center text-lg font-semibold text-white">
+					Your First Secret Smart Contract
 				</h2>
-				<p class="text-gray-600 text-center text-base">
+				<p class="text-center text-base text-gray">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed dapibus libero. Mauris et
 					orci vel nibh accumsan suscipit sit amet sit amet diam.
 				</p>
 			</div>
 		</div>
 
-		<div class="max-w-lg overflow-hidden rounded-lg bg-white px-6 py-6 shadow lg:px-8 lg:py-8">
+		<div class="max-w-lg overflow-hidden rounded-2xl bg-dark-4 px-6 py-6 shadow-lg lg:px-8 lg:py-8">
 			<div class="grid auto-rows-min">
 				<p class="mb-4 inline-block justify-self-end rounded-full bg-blue-400 px-3 py-1 text-white">
 					Beginner
 				</p>
 				<!-- <img src="" alt=""> -->
 				<div class="mx-auto mb-6 h-36 w-36 bg-slate-400" />
-				<h2 class="text-gray-900 mb-2 text-center text-lg font-semibold">
-					Building Your First Secret dApp
-				</h2>
-				<p class="text-gray-600 text-center text-base">
+				<h2 class="mb-2 text-center text-lg font-semibold text-white">Your First Secret dApp</h2>
+				<p class="text-center text-base text-gray">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed dapibus libero. Mauris et
 					orci vel nibh accumsan suscipit sit amet sit amet diam.
 				</p>
