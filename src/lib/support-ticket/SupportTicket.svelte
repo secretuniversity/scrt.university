bdc5c099ea4d5aed236e019ef8b5c7bc61596017
Alex Werner <alewern@pm.me>
2022-08-08 08:31:24 -0500

    task: adds base styles to support page

--- src/lib/support-ticket/SupportTicket.svelte
+++ src/lib/support-ticket/SupportTicket.svelte
@@ -1,18 +1,18 @@
-<form class="divide-gray-200 space-y-8 divide-y">
-	<div class="divide-gray-200 space-y-8 divide-y sm:space-y-5">
+<form class="space-y-8 divide-y divide-gray">
+	<div class="space-y-8 divide-y divide-gray sm:space-y-5">
 		<div>
 			<div>
-				<h3 class="text-gray-900 text-lg font-medium leading-6">Support Ticket</h3>
-				<p class="text-gray-500 mt-1 max-w-2xl text-sm">
+				<h3 class="text-lg font-semibold leading-6 text-white">Support Ticket</h3>
+				<p class="mt-1 max-w-2xl text-sm text-gray">
 					This ticket will be sent directly to the Secret University team.
 				</p>
 			</div>
 
 			<div class="mt-6 space-y-6 sm:mt-5 sm:space-y-5">
 				<div
-					class="sm:border-gray-200 sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:pt-5"
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray sm:pt-5"
 				>
-					<label for="subject" class="text-gray-700 block text-sm font-medium sm:mt-px sm:pt-2">
+					<label for="subject" class="block text-sm font-medium text-off-white sm:mt-px sm:pt-2">
 						Subject
 					</label>
 					<div class="mt-1 sm:col-span-2 sm:mt-0">
@@ -20,21 +20,21 @@
 							id="subject"
 							name="subject"
 							type="text"
-							class="border-gray-300 block w-full max-w-lg rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+							class="block w-full max-w-lg rounded-md border-none bg-dark-3 text-white shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 						/>
 					</div>
 				</div>
 
 				<div
-					class="sm:border-gray-200 sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:pt-5"
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray sm:pt-5"
 				>
-					<label for="username" class="text-gray-700 block text-sm font-medium sm:mt-px sm:pt-2">
+					<label for="username" class="block text-sm font-medium text-off-white sm:mt-px sm:pt-2">
 						Topic
 					</label>
 					<div class="mt-1 sm:col-span-2 sm:mt-0">
 						<div class="flex max-w-lg rounded-md shadow-sm">
 							<span
-								class="border-gray-300 bg-gray-50 text-gray-500 inline-flex items-center rounded-l-md border border-r-0 px-3 sm:text-sm"
+								class="inline-flex items-center rounded-l-md border border-r-0 border-none bg-dark-2 px-3 text-gray sm:text-sm"
 							>
 								scrt.university/
 							</span>
@@ -43,16 +43,16 @@
 								name="username"
 								id="username"
 								autocomplete="username"
-								class="border-gray-300 block w-full min-w-0 flex-1 rounded-none rounded-r-md focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+								class="block w-full min-w-0 flex-1 rounded-none rounded-r-md border-none bg-dark-3 text-white focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 							/>
 						</div>
 					</div>
 				</div>
 
 				<div
-					class="sm:border-gray-200 sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:pt-5"
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray sm:pt-5"
 				>
-					<label for="email" class="text-gray-700 block text-sm font-medium sm:mt-px sm:pt-2">
+					<label for="email" class="block text-sm font-medium text-off-white sm:mt-px sm:pt-2">
 						Email address
 					</label>
 					<div class="mt-1 sm:col-span-2 sm:mt-0">
@@ -61,15 +61,15 @@
 							name="email"
 							type="email"
 							autocomplete="email"
-							class="border-gray-300 block w-full max-w-lg rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+							class="block w-full max-w-lg rounded-md border-none bg-dark-3 text-white shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 						/>
 					</div>
 				</div>
 
 				<div
-					class="sm:border-gray-200 sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:pt-5"
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray sm:pt-5"
 				>
-					<label for="about" class="text-gray-700 block text-sm font-medium sm:mt-px sm:pt-2">
+					<label for="about" class="block text-sm font-medium text-off-white sm:mt-px sm:pt-2">
 						Description
 					</label>
 					<div class="mt-1 sm:col-span-2 sm:mt-0">
@@ -77,21 +77,21 @@
 							id="about"
 							name="about"
 							rows="3"
-							class="border-gray-300 block w-full max-w-lg rounded-md border shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+							class="block w-full max-w-lg rounded-md border border-none bg-dark-3 text-white shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 						/>
-						<p class="text-gray-500 mt-2 text-sm">Describe your problem in a couple sentences.</p>
+						<p class="mt-2 text-sm text-gray">Describe your problem in a couple sentences.</p>
 					</div>
 				</div>
 
 				<div
-					class="sm:border-gray-200 sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:pt-5"
+					class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray sm:pt-5"
 				>
-					<label for="os" class="text-gray-700 block text-sm font-medium">Operating System</label>
+					<label for="os" class="block text-sm font-medium text-off-white">Operating System</label>
 					<div class="mt-1 sm:col-span-2 sm:mt-0">
 						<select
 							id="os"
 							name="os"
-							class="border-gray-300 block w-full max-w-lg rounded-md border shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+							class="block w-full max-w-lg rounded-md border border-none bg-dark-3 text-white shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 						>
 							<option>Windows</option>
 							<option>Mac OS</option>
@@ -109,12 +109,12 @@
 		<div class="flex justify-end">
 			<button
 				type="button"
-				class="border-gray-300 text-gray-700 hover:bg-gray-50 rounded-md border bg-white py-2 px-4 text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				class="rounded-md bg-white py-2 px-4 text-sm font-medium text-dark-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 				>Clear</button
 			>
 			<button
 				type="submit"
-				class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-dark-blue py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 				>Submit</button
 			>
 		</div>
--- src/routes/support.svelte
+++ src/routes/support.svelte
@@ -9,25 +9,25 @@
 
 <div class="grid grid-cols-1 items-center justify-items-center pt-16 pb-24 lg:grid-cols-2">
 	<div class="max-w-2xl flex-col py-16 lg:max-w-3xl lg:px-8">
-		<h1 class="text-gray-900 self-start text-3xl font-bold">Need Support?</h1>
-		<p class="text-gray-600 my-4 text-base">
+		<h1 class="self-start text-3xl font-bold text-white">Need Support?</h1>
+		<p class="my-4 text-base text-gray">
 			Are you having trouble working through a course or guide. We're here to help. Submit a support
 			ticket and we will get back to you soon. If you need faster support, consider asking your
 			question in on Secret Network's many community channels.
 		</p>
 		<a
 			href="/support#community"
-			class="inline-flex items-center justify-self-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+			class="inline-flex items-center justify-self-center rounded-md border border-transparent bg-dark-blue px-4 py-2 text-base font-medium text-white hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 			>Join The Community</a
 		>
 	</div>
 
-	<div class="w-9/12 overflow-hidden rounded-lg px-12 py-14 shadow-lg">
+	<div class="w-9/12 overflow-hidden rounded-lg bg-dark-4 px-12 py-14 shadow-lg">
 		<SupportTicket />
 	</div>
 </div>
 
-<div id="community" class="mb-36">
+<div id="community" class="bg-dark-2 pb-36">
 	<SectionHeader
 		title={'Secret Community'}
 		subtitle={'Join the community'}
@@ -35,42 +35,42 @@
 	/>
 
 	<div class="mx-auto mt-8 flex max-w-7xl gap-x-12">
-		<div class="lg:w-4/12">
+		<div class="flex flex-col items-center lg:w-4/12">
 			<!-- <img src="" alt=""> -->
-			<h2 class="text-gray-900 mb-3 text-center text-lg font-semibold">Chat on Discord</h2>
-			<p class="text-gray-600 mb-4 text-center text-base">
+			<h2 class="mb-3 text-lg font-semibold text-white">Chat on Discord</h2>
+			<p class="mb-4 text-center text-base text-gray">
 				Have real-time discussions with developers building on Secret Network
 			</p>
 			<a
 				href="#"
-				class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				class="inline-flex items-center rounded-md border border-transparent bg-light-cream px-4 py-2 text-base font-medium text-dark-2 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 				>Join Discord</a
 			>
 		</div>
 
-		<div class="lg:w-4/12">
+		<div class="flex flex-col items-center lg:w-4/12">
 			<!-- <img src="" alt=""> -->
-			<h2 class="text-gray-900 mb-3 text-center text-lg font-semibold">Post on the Forum</h2>
-			<p class="text-gray-600 mb-4 text-center text-base">
+			<h2 class="mb-3 text-lg font-semibold text-white">Post on the Forum</h2>
+			<p class="mb-4 text-center text-base text-gray">
 				Have real-time discussions with developers building on Secret Network
 			</p>
 			<a
 				href="#"
-				class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-				>Join Discussions</a
+				class="inline-flex items-center rounded-md border border-transparent bg-light-cream px-4 py-2 text-base font-medium text-dark-2 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				>Join Discussion</a
 			>
 		</div>
 
-		<div class="lg:w-4/12">
+		<div class="flex flex-col items-center lg:w-4/12">
 			<!-- <img src="" alt=""> -->
-			<h2 class="text-gray-900 mb-3 text-center text-lg font-semibold">Find us on Github</h2>
-			<p class="text-gray-600 mb-4 text-center text-base">
+			<h2 class="mb-3 text-lg font-semibold text-white">Find us on Github</h2>
+			<p class="mb-4 text-center text-base text-gray">
 				Have real-time discussions with developers building on Secret Network
 			</p>
 			<a
 				href="#"
-				class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-				>Open an Issue</a
+				class="inline-flex items-center rounded-md border border-transparent bg-light-cream px-4 py-2 text-base font-medium text-dark-2 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				>Check Github</a
 			>
 		</div>
 	</div>
