eb787b592dedb399487b1d23f0cc86d855cd8807
Alex Werner <alewern@pm.me>
2022-08-08 11:10:06 -0500

    fix: adds secret logo to nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -7,11 +7,7 @@
 			<div class="flex w-full items-center justify-between md:w-auto">
 				<a href="/">
 					<span class="sr-only">Workflow</span>
-					<img
-						class="h-8 w-auto sm:h-10"
-						src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-						alt=""
-					/>
+					<img class="h-10 w-auto" src="Logo White.svg" alt="Secret Network Logo" />
 				</a>
 				<div class="-mr-2 flex items-center md:hidden">
 					<button
