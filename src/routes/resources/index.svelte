dc1e63e40b6cbde74a527cbc3322400404bac55d
Alex Werner <alewern@pm.me>
2022-08-08 10:43:38 -0500

    task: adds temp base styles to community resources page

--- src/routes/resources/index.svelte
+++ src/routes/resources/index.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import Head from '$lib/Head.svelte';
 	import PageHeader from '$lib/PageHeader.svelte';
 	import Dropdown from '$lib/Dropdown.svelte';
+	import Repo from '$lib/RepoCard.svelte';
+
+	const pageTitle = 'Community Resources';
 </script>
+
+<Head {pageTitle} />
 
 <section class="mx-24">
 	<PageHeader
@@ -9,153 +15,61 @@
 		description={'Learn from the community. Find guides, courses, videos, and more— each made by developers on Secret Network.'}
 	/>
 
-	<div class="my-12 grid grid-cols-2">
-		<!-- Search Input -->
-		<div class="lg:max-w-2xl">
-			<label for="search" class="sr-only">Search</label>
-			<input
-				type="search"
-				name="search"
-				id="search"
-				class="border-gray-300 block w-full rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-				placeholder="Search [by #tag]"
-			/>
+	<div class="mt-12 grid grid-cols-4 gap-x-8 pb-12">
+		<div class="col-span-1">
+			<div class="mb-0.5 rounded-t-xl bg-dark-4 px-4 py-4 text-lg font-bold text-white">Filter</div>
+			<div class="mb-0.5 bg-dark-4 px-4 py-4 text-lg font-bold text-white">Type</div>
+			<div class="bg-dark-4 px-4 py-4 text-lg font-bold text-white">Tags</div>
 		</div>
 
-		<div class="self-center justify-self-end">
-			<Dropdown />
-		</div>
-	</div>
-
-	<!-- Repo Cards -->
-	<div class="bg-white px-4 pt-6 pb-20 sm:px-6 lg:px-8 lg:pt-8 lg:pb-28">
-		<div class="divide-gray-200 relative mx-auto max-w-lg divide-y-2 lg:mx-16 lg:max-w-full">
-			<div class="grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
-				<div class="overflow-hidden rounded-lg bg-white px-4 py-6 shadow lg:px-8 lg:py-4">
-					<div>
-						<!-- Tags -->
-						<a href="#" class="inline-block">
-							<span
-								class="inline-flex items-center rounded-full bg-indigo-100 px-3 py-0.5 text-sm font-medium text-indigo-800"
-							>
-								Article
-							</span>
-						</a>
-					</div>
-
-					<!-- Title and Description -->
-					<a href="#" class="mt-4 block">
-						<p class="text-gray-900 text-xl font-semibold">Boost your conversion rate</p>
-						<p class="text-gray-500 mt-3 text-base">
-							Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet.
-							Sapien tortor lacus arcu.
-						</p>
-					</a>
-					<div class="mt-6 flex items-center">
-						<div class="flex-shrink-0">
-							<a href="#">
-								<span class="sr-only">Paul York</span>
-								<img
-									class="h-10 w-10 rounded-full"
-									src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-									alt=""
-								/>
-							</a>
-						</div>
-						<div class="ml-3">
-							<p class="text-gray-900 text-sm font-medium">
-								<a href="#"> Paul York </a>
-							</p>
-							<div class="text-gray-500 flex space-x-1 text-sm">
-								<time datetime="2020-03-16"> Mar 16, 2020 </time>
-								<span aria-hidden="true"> &middot; </span>
-								<span> 6 min read </span>
-							</div>
-						</div>
-					</div>
+		<div class="col-span-3">
+			<div class="flex gap-x-4">
+				<!-- Search Input -->
+				<div class="w-full">
+					<label for="search" class="sr-only">Search</label>
+					<input
+						type="search"
+						name="search"
+						id="search"
+						class="block w-full rounded-md border-gray bg-dark-3 text-off-white shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						placeholder="Search for term [or #tag]"
+					/>
 				</div>
 
-				<div class="overflow-hidden rounded-lg bg-white px-4 py-6 shadow lg:px-8 lg:py-4">
-					<div>
-						<a href="#" class="inline-block">
-							<span
-								class="inline-flex items-center rounded-full bg-pink-100 px-3 py-0.5 text-sm font-medium text-pink-800"
-							>
-								Video
-							</span>
-						</a>
-					</div>
-					<a href="#" class="mt-4 block">
-						<p class="text-gray-900 text-xl font-semibold">
-							How to use search engine optimization to drive sales
-						</p>
-						<p class="text-gray-500 mt-3 text-base">
-							Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet.
-							Sapien tortor lacus arcu.
-						</p>
-					</a>
-					<div class="mt-6 flex items-center">
-						<div class="flex-shrink-0">
-							<a href="#">
-								<span class="sr-only">Dessie Ryan</span>
-								<img
-									class="h-10 w-10 rounded-full"
-									src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-									alt=""
-								/>
-							</a>
-						</div>
-						<div class="ml-3">
-							<p class="text-gray-900 text-sm font-medium">
-								<a href="#"> Dessie Ryan </a>
-							</p>
-							<div class="text-gray-500 flex space-x-1 text-sm">
-								<time datetime="2020-03-10"> Mar 10, 2020 </time>
-								<span aria-hidden="true"> &middot; </span>
-								<span> 4 min read </span>
-							</div>
-						</div>
-					</div>
-				</div>
+				<!-- <div class="self-center justify-self-end">
+					<Dropdown />
+				</div> -->
+			</div>
+			<div class="mt-4 flex gap-x-4">
+				<p class="inline-block rounded-full bg-dark-blue py-1 px-4 text-white">
+					&times<span class="pl-1 font-bold">tag:</span> secret-box
+				</p>
+				<p class="inline-block rounded-full bg-dark-blue py-1 px-4 text-white">
+					&times<span class="pl-1 font-bold">tag:</span> essential
+				</p>
+			</div>
+			<!-- Repo Cards -->
+			<div class="-z-10 pt-6 pb-20 lg:pt-4 lg:pb-28">
+				<div class="divide-gray-200 relative mx-auto max-w-lg divide-y-2 lg:max-w-full">
+					<div class="grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
+						<Repo
+							tags={['secret-box', 'essential']}
+							title={'Secret Counter'}
+							description={'A quick introdcution to building private smart contracts on Secret Network'}
+						/>
+						<Repo
+							tags={['library', 'smart-contracts', 'rust', 'essential']}
+							title={'secret-toolkit'}
+							description={'A rust package to help you build private smart contracts on Secret Network'}
+						/>
+						<Repo
+							tags={['smart-contracts', 'nfts']}
+							title={'secret-random-minting-snip721-impl'}
+							description={'A smart contract optimized for randomly minting SNIP-721s (Secret NFTs)'}
+						/>
 
-				<div class="overflow-hidden rounded-lg bg-white px-4 py-6 shadow lg:px-8 lg:py-4">
-					<div>
-						<a href="#" class="inline-block">
-							<span
-								class="inline-flex items-center rounded-full bg-green-100 px-3 py-0.5 text-sm font-medium text-green-800"
-							>
-								Case Study
-							</span>
-						</a>
-					</div>
-					<a href="#" class="mt-4 block">
-						<p class="text-gray-900 text-xl font-semibold">Improve your customer experience</p>
-						<p class="text-gray-500 mt-3 text-base">
-							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab iure iusto fugiat commodi
-							sequi.
-						</p>
-					</a>
-					<div class="mt-6 flex items-center">
-						<div class="flex-shrink-0">
-							<a href="#">
-								<span class="sr-only">Easer Collins</span>
-								<img
-									class="h-10 w-10 rounded-full"
-									src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-									alt=""
-								/>
-							</a>
-						</div>
-						<div class="ml-3">
-							<p class="text-gray-900 text-sm font-medium">
-								<a href="#"> Easer Collins </a>
-							</p>
-							<div class="text-gray-500 flex space-x-1 text-sm">
-								<time datetime="2020-02-12"> Feb 12, 2020 </time>
-								<span aria-hidden="true"> &middot; </span>
-								<span> 11 min read </span>
-							</div>
-						</div>
+						<Repo />
+						<Repo />
 					</div>
 				</div>
 			</div>
