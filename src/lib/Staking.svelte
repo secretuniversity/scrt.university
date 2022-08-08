aec268c8dfa6d8272c7673868f3b8c505250b060
Alex Werner <alewern@pm.me>
2022-08-08 08:02:03 -0500

    task: adds base styles to contribute page

--- src/lib/Staking.svelte
+++ src/lib/Staking.svelte
@@ -1,15 +1,15 @@
 <div class="container mx-auto px-4 sm:px-6 lg:py-20 lg:px-16">
-	<div class="overflow-hidden rounded-lg bg-white px-4 py-6 shadow lg:px-8 lg:py-4">
+	<div class="overflow-hidden rounded-lg bg-dark-4 px-4 py-6 shadow-lg lg:px-16 lg:py-12">
 		<!-- Content goes here -->
-		<h2 class="text-gray-900 text-2xl font-semibold leading-6">Stake With Us</h2>
-		<p class="text-gray-500 mt-2 text-base lg:max-w-xl">
+		<h2 class="text-2xl font-semibold leading-6 text-white">Stake With Us</h2>
+		<p class="mt-2 text-base text-gray lg:max-w-xl">
 			Secret University is committed to supporting developer growth and education on Secret Network.
 			By staking your SCRT with us, you are supporting the university's operating costs as well as
 			helping to fund developer, education-focused bounties.
 		</p>
 
 		<div class="my-4 max-w-lg">
-			<label for="amount" class="text-gray-700 block text-sm font-medium">Amount</label>
+			<label for="amount" class="block text-sm font-medium text-off-white">Amount</label>
 			<div class="relative mt-1 rounded-md shadow-sm">
 				<input
 					type="text"
@@ -20,14 +20,14 @@
 					aria-describedby="price-currency"
 				/>
 				<div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-					<span class="text-gray-500 sm:text-sm" id="amount-currency"> SCRT </span>
+					<span class="text-dark-4 sm:text-sm" id="amount-currency"> SCRT </span>
 				</div>
 			</div>
 		</div>
 
 		<button
 			type="button"
-			class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+			class="inline-flex items-center rounded-md border border-transparent bg-dark-blue px-4 py-2 text-base font-medium text-white hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 			>Stake</button
 		>
 	</div>
--- src/routes/contribute.svelte
+++ src/routes/contribute.svelte
@@ -8,8 +8,8 @@
 <Head {pageTitle} />
 
 <div class="mx-auto max-w-xl py-20">
-	<h1 class="text-gray-900 text-center text-6xl font-bold">Learn to Earn</h1>
-	<p class="text-gray-600 mt-2 text-center text-base">
+	<h1 class="text-center text-6xl font-bold text-white">Learn to Earn</h1>
+	<p class="mt-2 text-center text-base text-gray">
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel laoreet ipsum. Quisque et
 		nulla dapibus, vehicula odio mollis, vestibulum dolor. Cras hendrerit ipsum ut tempus
 		scelerisque.
@@ -18,28 +18,28 @@
 
 <div class="flex justify-center gap-x-24">
 	<div class="lg:max-w-xs">
-		<div class="bg-gray-500 mx-auto h-24 w-24" />
+		<div class="mx-auto h-24 w-24 bg-dark-blue" />
 		<!-- <img src="" alt="" /> -->
-		<h2 class="text-gray-900 mt-3 text-center text-lg font-semibold">Code</h2>
-		<p class="text-gray-600 mt-1 text-center text-base">
+		<h2 class="mt-3 text-center text-lg font-semibold text-white">Code</h2>
+		<p class="mt-1 text-center text-base text-gray">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit
 		</p>
 	</div>
 
 	<div class="lg:max-w-xs">
-		<div class="bg-gray-500 mx-auto h-24 w-24" />
+		<div class="mx-auto h-24 w-24 bg-dark-blue" />
 		<!-- <img src="" alt="" /> -->
-		<h2 class="text-gray-900 mt-3 text-center text-lg font-semibold">Create</h2>
-		<p class="text-gray-600 mt-1 text-center text-base">
+		<h2 class="mt-3 text-center text-lg font-semibold text-white">Create</h2>
+		<p class="mt-1 text-center text-base text-gray">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit
 		</p>
 	</div>
 
 	<div class="lg:max-w-xs">
-		<div class="bg-gray-500 mx-auto h-24 w-24" />
+		<div class="mx-auto h-24 w-24 bg-dark-blue" />
 		<!-- <img src="" alt="" /> -->
-		<h2 class="text-gray-900 mt-3 text-center text-lg font-semibold">Teach</h2>
-		<p class="text-gray-600 mt-1 text-center text-base">
+		<h2 class="mt-3 text-center text-lg font-semibold text-white">Teach</h2>
+		<p class="mt-1 text-center text-base text-gray">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit
 		</p>
 	</div>
@@ -48,7 +48,7 @@
 <div class="my-12 flex justify-center">
 	<button
 		type="button"
-		class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+		class="inline-flex items-center rounded-md border border-transparent bg-dark-blue px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 		>Apply</button
 	>
 </div>
@@ -58,13 +58,13 @@
 <SectionHeader
 	title={'Community Curated Bounties'}
 	subtitle={'Looking for your next project?'}
-	description={"If you're ready to build, take a look at Secret Network's Community Curated Bounty List (CCBL) and build what the community is asking for."}
+	description={"If you're ready, try building for the community. Use the CCBL to find your next project on Secret Network."}
 />
 
-<div class="mb-32 flex justify-center">
+<div class="flex justify-center bg-dark-2 pb-32">
 	<button
 		type="button"
-		class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+		class="inline-flex items-center rounded-md border border-transparent bg-light-cream px-6 py-3 text-base font-medium text-dark-2 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 		>Learn more</button
 	>
 </div>
