529d403794f72c590da0029e471656ab5bd33fc5
Alex Werner <alewern@pm.me>
2022-09-24 12:38:13 -0500

    task: adds user store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,9 +1,23 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 import type { SecretNetworkClient } from 'secretjs';
+import type { Bookmark } from './models/index'
 
-interface SecretClientWrapper {
+interface UserStore {
+	pubAddress: string | null,
+	profileImgUrl: string | null,
+	bookmarks: Array<Bookmark> | null
+}
+
+
+interface SecretClientStore {
 	client: SecretNetworkClient | null;
 }
 
-export const secret: Writable<SecretClientWrapper> = writable({ client: null });
+
+export const secret: Writable<SecretClientStore> = writable({ client: null });
+export const user: Writable<UserStore> = writable({ 
+	pubAddress: null, 
+	profileImgUrl: null, 
+	bookmarks: null 
+})
