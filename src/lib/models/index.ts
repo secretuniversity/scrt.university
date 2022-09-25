5118d6affead753b377cc24a523c6408a794cb5a
Alex Werner <alewern@pm.me>
2022-09-24 20:47:51 -0500

    task: adds basic user to models

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -1,78 +1,83 @@
+export interface User {
+	id: number;
+	address: string;
+}
+
 export interface Tag {
-    id: number,
-    name: string
+	id: number;
+	name: string;
 }
 export interface Article {
-    id: number,
-    title: string,
-    description: string,
-    contributor: number,
-    content: string,
-    "created_at": string,
-    "updated_at": string,
-    tags: Array<Tag>
+	id: number;
+	title: string;
+	description: string;
+	contributor: number;
+	content: string;
+	created_at: string;
+	updated_at: string;
+	tags: Array<Tag>;
 }
 
 export interface Bookmark {
-    repos: Array<Repo>,
-    boxes: Array<SecretBox>,
-    pathways: Array<Pathway>,
-    articles: Array<Article>,
-    videos: Array<Video>
+	repos: Array<Repo>;
+	boxes: Array<SecretBox>;
+	pathways: Array<Pathway>;
+	articles: Array<Article>;
+	videos: Array<Video>;
 }
 
 export interface Repo {
-    id: number,
-    title: string,
-    description: string,
-    "github_username": string,
-    url: string,
-    "created_at": string,
-    "updated_at": string,
-    tags: Array<Tag>
+	id: number;
+	title: string;
+	description: string;
+	github_username: string;
+	url: string;
+	created_at: string;
+	updated_at: string;
+	tags: Array<Tag>;
 }
 
 export interface SecretBox {
-    id: number,
-    title: string,
-    description: string,
-    contributor: string,
-    url: string,
-    difficulty: string,
-    "dev_env": string,
-    "banner_img": string,
-    "created_at": string,
-    "updated_at": string,
-    tags: Array<Tag>
+	id: number;
+	title: string;
+	description: string;
+	contributor: string;
+	url: string;
+	difficulty: string;
+	dev_env: string;
+	banner_img: string;
+	created_at: string;
+	updated_at: string;
+	tags: Array<Tag>;
 }
 
 export interface Video {
-    id: number,
-    title: string,
-    description: string,
-    contributor: string,
-    file: string,
-    "created_at": string,
-    "updated_at": string,
-    // tags: Array<Tag>
+	id: number;
+	title: string;
+	description: string;
+	contributor: string;
+	file: string;
+	created_at: string;
+	updated_at: string;
+	// tags: Array<Tag>
 }
 
 export interface Bounty {
 	id: string;
 	title: string;
 	description: string;
-	"forum_url": string;
-	"proposal_url": string | null;
+	forum_url: string;
+	proposal_url: string | null;
 	amount: number;
 	status: string;
-    "created_at": string,
-    "updated_at": string,
-};
+	created_at: string;
+	updated_at: string;
+}
 
 export interface Contributor {
-    id: number,
-    address: string,
-    name: string
+	id: number;
+	address: string;
+	name: string;
 }
 
 // type BountyStatus = 'open' | 'completed' | 'in-progress';
@@ -89,22 +94,24 @@ export interface Pathway {
 }
 
 export interface Lesson {
-    id: string,
-    name: string, 
-    content: string,
-    "pathway_id": number,
+	id: string;
+	name: string;
+	content: string;
+	pathway_id: number;
 }
 
 export interface Quiz {
-    id: string,
-    question: string, 
-    answer: string,
-    hint: string,
-    "lesson_id": number,
+	id: string;
+	question: string;
+	answer: string;
+	hint: string;
+	lesson_id: number;
 }
 
 export interface QuizOption {
-    id: string,
-    content: string,
-    "quiz_id": number,
+	id: string;
+	content: string;
+	quiz_id: number;
 }
+
+export type Contribution = Repo | SecretBox | Pathway | Video | Article;
