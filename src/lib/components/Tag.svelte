1d76c80bc6db795c8edc7167a1e6f93771b62251
Alex Werner <alewern@pm.me>
2022-09-30 22:47:08 -0500

    task: creates tag component

---
+++ src/lib/components/Tag.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Tag } from '$lib/models/index';
+	export let tag: Tag;
+</script>
+
+<span
+	class="inline-flex items-center rounded-full bg-gray px-3 py-0.5 text-sm font-medium text-dark-4"
+>
+	{tag.name}
+</span>
