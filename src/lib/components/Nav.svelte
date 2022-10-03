c76ed2aa6923fa57e856bca4d7bb0bf4bf29c924
Alex Werner <alewern@pm.me>
2022-10-03 10:56:20 -0500

    fix: removes mobile support

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -133,7 +133,7 @@
 							class="absolute top-0 left-0 z-10 mt-10 w-max rounded-md border border-gray bg-dark-4 p-4"
 						>
 							<div class="grid-col-1 grid gap-y-2 overflow-visible border-gray text-white">
-								<a href="#">Application</a>
+								<a href="/build">Application</a>
 								<a href="/bounties">Bounties</a>
 							</div>
 						</div>
@@ -169,79 +169,4 @@
 			</span>
 		</div>
 	</nav>
-
-	<!--
-	        Mobile menu, show/hide based on menu open state.
-
-	        Entering: "duration-150 ease-out"
-	          From: "opacity-0 scale-95"
-	          To: "opacity-100 scale-100"
-	        Leaving: "duration-100 ease-in"
-	          From: "opacity-100 scale-100"
-	          To: "opacity-0 scale-95"
-	      -->
-	<div class="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden">
-		<div class="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
-			<div class="flex items-center justify-between px-5 pt-4">
-				<div>
-					<img
-						class="h-8 w-auto"
-						src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-						alt=""
-					/>
-				</div>
-				<div class="-mr-2">
-					<button
-						type="button"
-						class="text-gray-400 hover:bg-gray-100 hover:text-gray-500 inline-flex items-center justify-center rounded-md bg-white p-2 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-					>
-						<span class="sr-only">Close main menu</span>
-						<!-- Heroicon name: outline/x -->
-						<svg
-							class="h-6 w-6"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="2"
-							stroke="currentColor"
-							aria-hidden="true"
-						>
-							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-						</svg>
-					</button>
-				</div>
-			</div>
-			<div class="space-y-1 px-2 pt-2 pb-3">
-				<a
-					href="#"
-					class="text-gray-700 hover:bg-gray-50 hover:text-gray-900 block rounded-md px-3 py-2 text-base font-medium"
-					>Product</a
-				>
-
-				<a
-					href="#"
-					class="text-gray-700 hover:bg-gray-50 hover:text-gray-900 block rounded-md px-3 py-2 text-base font-medium"
-					>Features</a
-				>
-
-				<a
-					href="#"
-					class="text-gray-700 hover:bg-gray-50 hover:text-gray-900 block rounded-md px-3 py-2 text-base font-medium"
-					>Marketplace</a
-				>
-
-				<a
-					href="#"
-					class="text-gray-700 hover:bg-gray-50 hover:text-gray-900 block rounded-md px-3 py-2 text-base font-medium"
-					>Company</a
-				>
-			</div>
-			<a
-				href="#"
-				class="bg-gray-50 hover:bg-gray-100 block w-full px-5 py-3 text-center font-medium text-indigo-600"
-			>
-				Log in
-			</a>
-		</div>
-	</div>
 </div>
