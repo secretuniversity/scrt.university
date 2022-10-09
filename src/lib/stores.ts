1af2f63e88219dc43f0fdb22e4ec1906704571ab
Alex Werner <alewern@pm.me>
2022-10-09 10:37:46 -0500

    task: adds pathwayRequest store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 import type { SecretNetworkClient } from 'secretjs';
-import type { Bookmark, Contribution, User } from './models/index';
+import type { Bookmark, Contribution, PathwayRequest, User } from './models/index';
 
 interface ContributionStore {
 	data: Array<Contribution>;
@@ -19,3 +19,10 @@ export const secret: Writable<SecretClientStore> = writable({ client: null });
 export const user: Writable<User> = writable({ id: 0, address: '' });
 export const bookmarks: Writable<BookmarkStore> = writable({ data: [] });
 export const contributions: Writable<ContributionStore> = writable({ data: [] });
+export const pathwayRequest: Writable<PathwayRequest> = writable({
+	title: '',
+	contributor: -1,
+	description: '',
+	difficulty: '',
+	lessons: []
+} as PathwayRequest)
