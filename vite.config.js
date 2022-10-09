ee0ecca9dd84d5d37391e4e54a7d26ff3ddfef19
Alex Werner <alewern@pm.me>
2022-10-09 10:33:33 -0500

    task: adds api proxy for local development

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:3000/',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	}
 };
 
 export default config;
