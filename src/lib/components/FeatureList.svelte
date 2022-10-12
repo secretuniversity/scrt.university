c15e7056d94f10ac5196856d86a1e7245d0e1fab
Alex Werner <alewern@pm.me>
2022-10-11 20:50:54 -0500

    fix: removes unneeded styles

--- src/lib/components/FeatureList.svelte
+++ src/lib/components/FeatureList.svelte
@@ -14,7 +14,7 @@
 	export let srText = '';
 </script>
 
-<div class="border-1 border-solid bg-dark-2 pt-6 pb-36">
+<div class="border-1 border-solid pt-6 pb-36">
 	<div class="mx-32 max-w-full">
 		<h2 class="sr-only">{srText}</h2>
 		<dl class="space-y-10 lg:grid lg:grid-cols-3 lg:gap-12 lg:space-y-0">
