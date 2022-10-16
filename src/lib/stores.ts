c2b72d858cd2245ecb57150d143c0759d782c439
Alex Werner <alewern@pm.me>
2022-10-15 21:08:33 -0500

    task: simplifies svelte stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,28 +1,11 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 import type { SecretNetworkClient } from 'secretjs';
-import type { Bookmark, Contribution, PathwayRequest, User } from './models/index';
+import type { Bookmark, Contributor, Contribution, PathwayRequest, User } from './models/index';
 
-interface ContributionStore {
-	data: Array<Contribution>;
-}
-
-interface BookmarkStore {
-	data: Array<Bookmark>;
-}
-
-interface SecretClientStore {
-	client: SecretNetworkClient | null;
-}
-
-export const secret: Writable<SecretClientStore> = writable({ client: null });
-export const user: Writable<User> = writable({ id: -1, address: '' });
-export const bookmarks: Writable<BookmarkStore> = writable({ data: [] });
-export const contributions: Writable<ContributionStore> = writable({ data: [] });
-export const pathwayRequest: Writable<PathwayRequest> = writable({
-	title: '',
-	contributor: -1,
-	description: '',
-	difficulty: '',
-	lessons: []
-} as PathwayRequest);
+export const secret: Writable<SecretNetworkClient | null> = writable(null);
+export const user: Writable<User | null> = writable(null);
+export const bookmarks: Writable<Bookmark[] | null> = writable(null);
+export const contributions: Writable<Contribution[] | null> = writable(null);
+export const contributor: Writable<Contributor | null> = writable(null);
+export const pathwayRequest: Writable<PathwayRequest | null> = writable(null);
