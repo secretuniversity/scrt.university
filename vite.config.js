5f2053177205d59ee733fa98e1658fbf591b508c
Alex Werner <alewern@pm.me>
2022-11-04 11:31:31 -0500

    fix: updates deployment configs

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 
 /** @type {import('vite').UserConfig} */
-export default ({ mode }) => {
-	process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
-
-	const target = process.env.CELADON_URL;
-
-	if (!target) {
-		throw new Error('CELADON_URL is not set');
-	}
-	return defineConfig({
-		plugins: [sveltekit(), EnvironmentPlugin('all', { loadEnvFiles: false })],
-		server: {
-			proxy: {
-				'/api': {
-					target,
-					changeOrigin: true,
-					rewrite: (path) => path.replace(/^\/api/, '')
-				}
+export default defineConfig({
+	plugins: [sveltekit(), EnvironmentPlugin({ CELADON_URL: undefined }, { loadEnvFiles: false })],
+	server: {
+		proxy: {
+			'/api': {
+				target: process.env.CELADON_URL,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
 			}
 		}
-	});
-};
+	}
+});
