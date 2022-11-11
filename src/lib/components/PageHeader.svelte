c9408382b0d9b6ea34c5de950ec3ea886448cd04
Alex Werner <alewern@pm.me>
2022-11-11 10:59:43 -0600

    update PageHeader and SectionHeader component styles

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -4,15 +4,15 @@
 	export let image = '';
 </script>
 
-<div class="mt-8 rounded-2xl bg-dark-4 shadow-lg">
-	<div class="mx-auto flex max-h-fit max-w-7xl justify-between py-12">
-		<div class="grid h-full w-full grid-cols-2 grid-rows-2">
+<div class="rounded-3xl bg-dark-4 shadow-lg">
+	<div class="mx-auto h-[36rem] max-w-6xl py-20 md:px-16">
+		<div class="grid h-full w-full grid-cols-2 grid-rows-2 items-center">
 			<h2
 				class="col-start-1 row-start-1 self-end text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl"
 			>
 				{title}
 			</h2>
-			<p class="col-start-1 row-start-2 mt-5 max-w-xl text-xl text-gray">
+			<p class="col-start-1 row-start-2 mt-5 max-w-xl self-start text-xl text-gray">
 				{@html description}
 			</p>
 			{#if image}
--- src/lib/components/SectionHeader.svelte
+++ src/lib/components/SectionHeader.svelte
@@ -4,14 +4,16 @@
 	export let description = 'Description';
 </script>
 
-<div class="bg-dark-2">
+<div class="my-16 bg-dark-2">
 	<div class="mx-auto max-w-7xl">
 		<div class="text-center">
-			<h2 class="text-base font-semibold uppercase tracking-wide text-dark-turquoise-g">{title}</h2>
-			<p class="mt-1 text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
+			<h2 class="my-4 text-base font-semibold uppercase tracking-wide text-dark-turquoise-g">
+				{title}
+			</h2>
+			<p class="text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
 				{subtitle}
 			</p>
-			<p class="mx-auto mt-5 max-w-2xl text-xl text-gray">
+			<p class="mx-auto my-4 max-w-2xl text-base text-gray">
 				{description}
 			</p>
 		</div>
