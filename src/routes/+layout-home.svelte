d9afdc57f4c81c140939cf4ff329caef25c5fb4b
Alex Werner <alewern@pm.me>
2022-11-12 15:41:21 -0600

    adds styles for mobile device warning

--- src/routes/+layout-home.svelte
+++ src/routes/+layout-home.svelte
@@ -2,6 +2,7 @@
 	import Banner from '$lib/components/Banner.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Notification from '$lib/components/Notifications.svelte';
+	import MobileImage from '$lib/assets/illustrations/mobile.svg';
 	import '../app.css';
 
 	const minScreenWidth = 961;
@@ -15,7 +16,14 @@
 <header />
 
 {#if isMobile}
-	<div>Sorry! Secret University currently only supports desktop devices.</div>
+	<section class="h-screen bg-dark-4 text-white">
+		<div class="flex h-full">
+			<p class="mx-auto max-w-md self-center text-center">
+				<img class="mx-auto mb-10 h-72 w-72" src={MobileImage} alt="Mobile is not supported" />
+				Sorry! Secret University currently only supports desktop devices at this time.
+			</p>
+		</div>
+	</section>
 {:else}
 	<div class="relative">
 		<Notification />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Nav from '$lib/components/Nav.svelte';
 	import Notification from '$lib/components/Notifications.svelte';
+	import MobileImage from '$lib/assets/illustrations/mobile.svg';
 	import '../app.css';
 
 	const minScreenWidth = 961;
@@ -16,8 +17,14 @@
 <header />
 
 {#if isMobile}
-	<!-- content here -->
-	<div>Sorry! Secret University currently only supports desktop devices.</div>
+	<section class="h-screen bg-dark-4 text-white">
+		<div class="flex h-full">
+			<p class="mx-auto max-w-md self-center text-center">
+				<img class="mx-auto mb-10 h-72 w-72" src={MobileImage} alt="Mobile is not supported" />
+				Sorry! Secret University currently only supports desktop devices at this time.
+			</p>
+		</div>
+	</section>
 {:else}
 	<div class="relative">
 		<Notification />
