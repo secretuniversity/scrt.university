251369e95e5ce0c5c9300891e7f8f193db74f3bb
Alex Werner <alewern@pm.me>
2022-11-14 20:54:27 -0600

    updates vite config

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 
-console.log({
-	config: {
-		CELADON_URL: process.env.CELADON_URL,
-		PUBLIC_SECRET_NETWORK_CHAIN_ID: process.env.PUBLIC_SECRET_NETWORK_CHAIN_ID,
-		PUBLIC_SECRET_NETWORK_GRPC_URL: process.env.PUBLIC_SECRET_NETWORK_GRPC_URL
-	}
-});
-
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ command, mode }) => ({
 	plugins: [
 		sveltekit(),
 		EnvironmentPlugin({
+			APP_ENV: 'development',
 			PUBLIC_SECRET_NETWORK_CHAIN_ID: undefined,
 			PUBLIC_SECRET_NETWORK_GRPC_URL: undefined,
 			CELADON_URL: undefined
@@ -25,8 +18,18 @@ export default defineConfig(({ command, mode }) => ({
 			'/api': {
 				target: process.env.CELADON_URL,
 				changeOrigin: true,
+				secure: process.env.APP_ENV === 'production',
 				rewrite: (path) => path.replace(/^\/api/, '')
 			}
 		}
 	}
 }));
+
+console.log({
+	config: {
+		APP_ENV: process.env.APP_ENV,
+		CELADON_URL: process.env.CELADON_URL,
+		PUBLIC_SECRET_NETWORK_CHAIN_ID: process.env.PUBLIC_SECRET_NETWORK_CHAIN_ID,
+		PUBLIC_SECRET_NETWORK_GRPC_URL: process.env.PUBLIC_SECRET_NETWORK_GRPC_URL
+	}
+});
