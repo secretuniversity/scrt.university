cd4b9ed801e51bb33fe2298797db1792878b49a4
Alex Werner <alewern@pm.me>
2022-11-14 19:21:20 -0600

    adds logging to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 
+console.log({
+	config: {
+		CELADON_URL: process.env.CELADON_URL,
+		PUBLIC_SECRET_NETWORK_CHAIN_ID: process.env.PUBLIC_SECRET_NETWORK_CHAIN_ID,
+		PUBLIC_SECRET_NETWORK_GRPC_URL: process.env.PUBLIC_SECRET_NETWORK_GRPC_URL
+	}
+});
+
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ command, mode }) => ({
 	plugins: [
