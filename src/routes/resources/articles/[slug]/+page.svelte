fe8094732018dcb5e5a02298dad8d12e674d59b5
Alex Werner <alewern@pm.me>
2022-11-21 10:51:04 -0600

    fixes article page rendering

--- src/routes/resources/articles/[slug]/+page.svelte
+++ src/routes/resources/articles/[slug]/+page.svelte
@@ -1,40 +1,63 @@
 <script lang="ts">
+	import Tag from '$lib/components/Tag.svelte';
+	import hljs from 'highlight.js';
 	import { onMount } from 'svelte';
-	import { selectedArticle } from '$lib/stores';
+	import { getNotification } from '$lib/helpers';
+	import { notificationsStore, selectedArticle } from '$lib/stores';
 	import { goto } from '$app/navigation';
-	import hljs from 'highlight.js/lib/core';
-	import javascript from 'highlight.js/lib/languages/javascript';
-	import html from 'highlight.js/lib/languages/xml';
-	import css from 'highlight.js/lib/languages/css';
-	import ts from 'highlight.js/lib/languages/typescript';
-	import rust from 'highlight.js/lib/languages/rust';
-	import bash from 'highlight.js/lib/languages/bash';
-	import python from 'highlight.js/lib/languages/python';
 
 	import 'highlight.js/styles/tokyo-night-dark.css';
+	import '$lib/styles/markdown.scss';
+	import { getBaseAPIUrl } from '$lib/helpers';
+
+	let author = '';
 
 	onMount(() => {
 		if (!$selectedArticle) {
 			goto('/resources');
+			return;
 		}
 
-		hljs.registerLanguage('js', javascript);
-		hljs.registerLanguage('html', html);
-		hljs.registerLanguage('css', css);
-		hljs.registerLanguage('ts', ts);
-		hljs.registerLanguage('rust', rust);
-		hljs.registerLanguage('sh', bash);
-		hljs.registerLanguage('py', python);
+		console.log($selectedArticle.tags);
+
+		fetchContributor();
 
 		hljs.highlightAll();
 	});
+
+	async function fetchContributor() {
+		if (!$selectedArticle) return;
+
+		try {
+			const url = getBaseAPIUrl() + `/v1/users/name/${$selectedArticle.contributor}`;
+			const res = await fetch(url);
+			const json = await res.json();
+
+			author = json;
+		} catch (_err) {
+			$notificationsStore = [
+				...$notificationsStore,
+				getNotification('Unable to find contributor name', 'error')
+			];
+		}
+	}
 </script>
 
 <section class="min-h-screen py-16 text-white">
 	{#if $selectedArticle}
 		<div class="flex h-full w-full flex-col items-center justify-center">
-			<p class="text-center text-5xl font-bold">{$selectedArticle.title}</p>
-			<div id="content" class="mt-8 w-full max-w-4xl">
+			<p class="my-6 text-center text-5xl font-bold">{$selectedArticle.title}</p>
+			<p class="mb-4 text-center text-base">Written by {author}</p>
+
+			<div class="mx-auto flex max-w-2xl flex-wrap space-x-2">
+				{#each $selectedArticle.tags as tag}
+					<div class="mb-2">
+						<Tag {tag} />
+					</div>
+				{/each}
+			</div>
+
+			<div class="markdown mt-8 w-full max-w-4xl">
 				{@html $selectedArticle.content}
 			</div>
 		</div>
@@ -46,7 +69,7 @@
 	{/if}
 </section>
 
-<style>
+<!-- <style>
 	#content :global(h1) {
 		@apply mb-2 text-4xl;
 	}
@@ -54,4 +77,4 @@
 	#content :global(pre) {
 		@apply my-2 rounded bg-dark-4 p-2;
 	}
-</style>
+</style> -->
