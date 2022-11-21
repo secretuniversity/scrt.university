8fcea1b2ba733f0fb93f9ab7b97614124010fa36
Alex Werner <alewern@pm.me>
2022-11-21 10:51:35 -0600

    fixes video page rendering

--- src/routes/resources/videos/[slug]/+page.svelte
+++ src/routes/resources/videos/[slug]/+page.svelte
@@ -1,38 +1,65 @@
 <script lang="ts">
-	import { selectedVideo } from '$lib/stores';
+	import Tag from '$lib/components/Tag.svelte';
+	import { goto } from '$app/navigation';
+	import { getBaseAPIUrl, getNotification } from '$lib/helpers';
+	import { notificationsStore, selectedVideo } from '$lib/stores';
 	import { onMount } from 'svelte';
 
-	let contributorName = '';
+	let author = '';
+	let src = '';
 
-	onMount(() => {
-		if ($selectedVideo) {
-			fetch(`/api/v1/contributor/id/${$selectedVideo.contributor}`)
-				.then((res) => res.json())
-				.then((res) => {
-					contributorName = res.data.name;
-				});
+	onMount(async () => {
+		if (!$selectedVideo) {
+			goto('/resources');
+			return;
+		}
+
+		try {
+			const url = getBaseAPIUrl() + `/v1/users/name/${$selectedVideo.contributor}`;
+			const res = await fetch(url);
+			const json = await res.json();
+
+			author = json;
+			console.log(process.env.APP_ENV);
+
+			const videoUrl = `https://storage.googleapis.com/celadon/${process.env.APP_ENV}/videos/${$selectedVideo.id}/${$selectedVideo.title}.mp4`;
+			const clean = videoUrl.replace(/ /g, '%20');
+			src = clean;
+
+			console.log(src);
+		} catch (err) {
+			$notificationsStore = [
+				...$notificationsStore,
+				getNotification("Could not find the name of this video's contributor", 'error')
+			];
 		}
 	});
 </script>
 
 <section class="min-h-screen">
 	{#if $selectedVideo}
-		<div class="mx-auto mt-16 max-w-5xl">
-			<div class="rounded-sm border-2 border-dark-5">
-				<video autoplay controls width="100%" height="auto">
-					<source
-						src="https://storage.googleapis.com/celadon/videos/{$selectedVideo.file}"
-						type="video/mp4"
-					/>
-				</video>
+		<div class="mx-auto mt-8 grid max-w-5xl grid-cols-1">
+			<div class="mb-8 flex w-fit justify-self-end">
+				{#each $selectedVideo.tags as tag}
+					<Tag {tag} />
+				{/each}
 			</div>
-			<div class="mt-4 flex h-12 items-center">
-				<h2 class="mr-4 text-2xl text-white">{$selectedVideo.title}</h2>
-				{#if contributorName}
-					<p class="text-sm text-white">by {contributorName}</p>
+
+			<div class="rounded-sm border-2 border-dark-5">
+				{#if src}
+					<video autoplay controls width="100%" height="auto">
+						<source {src} type="video/mp4" />
+					</video>
 				{/if}
 			</div>
-			<p class="mt-4 w-full rounded-md bg-dark-5 p-4 text-base text-white">
+
+			<div class="mt-4 grid h-12 grid-cols-2 items-center">
+				<h2 class="mr-4 text-2xl text-white">{$selectedVideo.title}</h2>
+				{#if author}
+					<p class="justify-self-end text-sm text-white">by {author}</p>
+				{/if}
+			</div>
+			<p class="mt-2 w-full rounded-md bg-dark-5 p-4 text-base text-white">
 				{$selectedVideo.description}
 			</p>
 		</div>
