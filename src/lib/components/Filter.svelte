9aa4ccf363e6376b91f8492ad9a49e2e68b2002d
Alex Werner <alewern@pm.me>
2022-11-27 21:03:20 -0600

    fixes filter caret image size bug

--- src/lib/components/Filter.svelte
+++ src/lib/components/Filter.svelte
@@ -18,13 +18,6 @@
 
 <div class="mb-0.5 flex rounded-t-xl bg-dark-4 p-4">
 	<p class="grow text-lg font-bold text-white">Filter</p>
-	<img
-		class="block cursor-pointer"
-		width="10"
-		height="10"
-		src={CaretLeftIcon}
-		alt="Hide the side filter"
-	/>
 </div>
 
 {#each sections as section, index}
@@ -32,27 +25,31 @@
 	<div class="mb-0.5 flex bg-dark-4 p-4">
 		<p class="grow text-lg font-bold text-white">{section}</p>
 		{#if flags[index]}
-			<img
-				on:click={() => {
-					flags[index] = false;
-				}}
-				class="block cursor-pointer"
-				width="15"
-				height="15"
-				src={CaretUpIcon}
-				alt="Hide the side filter"
-			/>
+			<div class="h-full self-center">
+				<img
+					on:click={() => {
+						flags[index] = false;
+					}}
+					width="20"
+					height="20"
+					class="block cursor-pointer"
+					src={CaretUpIcon}
+					alt="Hide the side filter"
+				/>
+			</div>
 		{:else}
-			<img
-				on:click={() => {
-					flags[index] = true;
-				}}
-				class="block cursor-pointer"
-				width="15"
-				height="15"
-				src={CaretDownIcon}
-				alt="Hide the side filter"
-			/>
+			<div class="h-full self-center">
+				<img
+					on:click={() => {
+						flags[index] = true;
+					}}
+					width="20"
+					height="20"
+					class="block cursor-pointer"
+					src={CaretDownIcon}
+					alt="Hide the side filter"
+				/>
+			</div>
 		{/if}
 	</div>
 
