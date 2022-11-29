e850f458aacca3383f4dab6d655a40796c4b4b07
Alex Werner <alewern@pm.me>
2022-11-29 13:53:49 -0600

    adds open source sans font

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -8,4 +8,10 @@
 		name="description"
 		content="Secret University is providing devs with the knowledge to build a private, decentralized web. Learn how you can join the university and help us to support our developers."
 	/>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
