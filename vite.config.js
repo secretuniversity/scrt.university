e3339276c516cc02f3a2770cee0c20bf221bea94
Alex Werner <alewern@pm.me>
2023-01-17 17:33:10 -0600

    updates site config for optimizations

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
+import progress from 'vite-plugin-progress';
 // import vitePluginImportus from 'vite-plugin-importus';
 
 /** @type {import('vite').UserConfig} */
 const config = defineConfig(({ command, mode }) => ({
 	plugins: [
+		progress(),
 		sveltekit(),
 		EnvironmentPlugin({
 			APP_ENV: 'development',
@@ -23,6 +25,19 @@ const config = defineConfig(({ command, mode }) => ({
 				target: 'http://127.0.0.1:3000',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	},
+	sourceMap: true,
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: (id) => {
+					// Magic happens here
+					if (id.includes('secretjs')) {
+						return 'vendor-secretjs';
+					}
+				}
 			}
 		}
 	}
