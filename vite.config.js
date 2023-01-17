b5458d0028c19caaa67dc965f176d15090e0cb68
Alex Werner <alewern@pm.me>
2023-01-17 00:00:23 -0600

    adds bundling plugins to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
+// import vitePluginImportus from 'vite-plugin-importus';
+import { compression } from 'vite-plugin-compression2';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 /** @type {import('vite').UserConfig} */
 const config = defineConfig(({ command, mode }) => ({
 	plugins: [
 		sveltekit(),
+		chunkSplitPlugin(),
 		EnvironmentPlugin({
 			APP_ENV: 'development',
 			PUBLIC_SECRET_NETWORK_CHAIN_ID: undefined,
 			PUBLIC_SECRET_NETWORK_GRPC_URL: undefined,
 			CELADON_URL: undefined
-		})
+		}),
+		compression()
 	],
 	optimizeDeps: {
 		include: ['highlight.js', 'highlight.js/lib/core']
