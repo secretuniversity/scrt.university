30f275579a933b141bac2baa597027555dab1973
Alex Werner <alewern@pm.me>
2023-03-23 22:43:08 -0500

    remove content req in article schema

--- src/lib/types/schemas/articles.ts
+++ src/lib/types/schemas/articles.ts
@@ -1,11 +1,11 @@
-import { array, number, object, string } from 'yup';
 import type { InferType } from 'yup';
+import { array, number, object, string } from 'yup';
 
 export const articleRequestSchema = object({
 	title: string().required(),
 	description: string().required(),
 	contributor: number().required().positive().integer(),
-	content: string().required(),
+	// content: string().required(),
 	tags: array().of(string().required())
 });
 
