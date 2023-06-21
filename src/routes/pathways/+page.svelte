dbd9263e54b00a903c0ad1d9510f0d999a96dbd9
Alex Werner <alewern@pm.me>
2023-06-21 09:16:34 -0500

    removes front end preview on pathways page

--- src/routes/pathways/+page.svelte
+++ src/routes/pathways/+page.svelte
@@ -118,30 +118,6 @@
 					</div>
 				</div>
 			{/each}
-
-			<div class="flex w-full {placeholderIndex % 2 === 0 ? 'justify-start' : 'justify-end'}">
-				<div class="w-[65%]">
-					<div
-						class="mb-4 rounded-md border-2 border-dark-4 bg-dark-3 p-6 px-8 text-white shadow-lg"
-					>
-						<div class="grid grid-cols-2 items-center">
-							<h2 class="pt-4 text-2xl font-semibold">
-								Leveling Up Your Front End with Secret University
-							</h2>
-							<p class="justify-self-end capitalize">Intermediate</p>
-						</div>
-						<p class="mt-2 max-w-xl">
-							In this pathway you'll learn how to develop efficient, lightweight web interfaces for
-							secret smart contracts. Using TypeScript we'll make a working front end for our dice
-							rolling smart contract and learn the ins and outs of interfacing with Secret Network.
-						</p>
-
-						<div class="grid w-full grid-cols-2 pb-4">
-							<p class="mt-2 cursor-pointer text-light-blue">Coming Soon</p>
-						</div>
-					</div>
-				</div>
-			</div>
 		</div>
 	</section>
 </div>
